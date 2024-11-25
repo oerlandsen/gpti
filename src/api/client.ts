09b998636ffef9dbb0723b1feb2f59429331b86d
oerlandsen <oerlandsen@uc.cl>
2024-11-25 20:19:02 -0300

    feat(mercadolibre): api auth

--- src/api/client.ts
+++ src/api/client.ts
@@ -2,6 +2,9 @@ import axios from 'axios';
 
 const client = axios.create({
   baseURL: 'https://api.mercadolibre.com',
+  headers: {
+    'Authorization': `Bearer APP_USR-92783577301737-112517-3374cea6a34159edcfd42baf2021aef1-48687107`,
+  },
 });
 
 export default client;
