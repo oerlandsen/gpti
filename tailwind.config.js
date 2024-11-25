af044dd4229932aa5054d509a367720d16f579bb
oerlandsen <oerlandsen@uc.cl>
2024-11-24 23:14:51 -0300

    feat: tailwind and form

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}", // Asegúrate de incluir los archivos correctos
+  ],
+  theme: {
+    extend: {}, // Puedes personalizar la configuración aquí si lo necesitas
+  },
+  plugins: [], // Agrega plugins si los necesitas
+};
