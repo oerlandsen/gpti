100e6a7b5ae7f06c0fee1eea62b8a34d21bcf1bd
oerlandsen <oerlandsen@uc.cl>
2024-11-26 23:34:20 -0300

    delete proxy

--- src/api/client.ts
+++ src/api/client.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const client = axios.create({
-  baseURL: '/api-mercadolibre',
+  baseURL: 'https://api.mercadolibre.com',
   headers: {
     'Authorization': `Bearer APP_USR-92783577301737-112621-7ac8a254c71f38e96334b833859c6076-48687107`,
   },
