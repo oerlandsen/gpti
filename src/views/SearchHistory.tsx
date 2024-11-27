c0250d53e3b5138220b333ab244550b305ff77bb
oerlandsen <oerlandsen@uc.cl>
2024-11-26 21:59:05 -0300

    feat: historial

--- src/views/SearchHistory.tsx
+++ src/views/SearchHistory.tsx
@@ -1,15 +1,61 @@
+import { useNavigate } from "react-router-dom";
 import { useAppContext } from "../AppContext";
+
+
 
 interface HistoryElement {
     searchTerm: string;
-    category: string;
+    searchDate: Date;
     imagePath: string;
     category: string;
-}
+  }
 
 function SearchHistory() {
 
-    const { history } = useAppContext();
+    const { history, setProductName, setCategoria, setFile } = useAppContext();
+    const navigate = useNavigate();
+
+    const categoriaParser = (name: string) => {
+        switch (name) {
+            case "amplificador"
+                : setCategoria("MLC1182")
+                break;
+            case "notebook"
+                : setCategoria("MLC1648")
+                break;
+            case "bajo"
+                : setCategoria("MLC1182")
+                break;
+            case "cien"
+                : setCategoria("MLC3025")
+                break;
+            case "rubik"
+                : setCategoria("MLC1132")
+                break;
+            case "totodile"
+                : setCategoria("MLC1132")
+                break;
+            default
+                : setCategoria("MLC1953")
+                break;
+        }
+    }
+
+    const handleClick = async (historyElement: any) => {
+        setProductName(historyElement.searchTerm);
+        console.log("asdasfas", historyElement.imagePath.split(".")[0].replace("/images/", ""));
+
+        categoriaParser(historyElement.imagePath.split(".")[0].replace("/images/", ""));
+        try {
+            const response = await fetch(historyElement.imagePath);
+            const blob = await response.blob();
+            setFile(blob);
+            navigate("/product");
+        } catch (error) {
+            console.error("Error fetching reviews:", error);
+        }
+    }
+
 
     return (
         <div>
@@ -20,6 +66,7 @@ function SearchHistory() {
                 <div className="p-6 bg-white rounded-xl shadow-lg w-full max-w-lg">
                     <ul className="space-y-6">
                         {history.map((historyElement: HistoryElement, index: number) => (
+                            <>
                             <li key={index} className="border-b border-gray-200 pb-4">
                                 <h2 className="text-xl font-semibold text-gray-700 mb-2">
                                     Producto: {historyElement.searchTerm}
@@ -32,7 +79,9 @@ function SearchHistory() {
                                     alt="Producto"
                                     className="w-full h-auto max-h-48 object-contain rounded-md"
                                 />
+                                <h2 onClick={() => handleClick(historyElement)} className="text-xl text-gray-800 hover:text-blue-600 cursor-pointer transition-colors mt-6 mb-4">Volver a Buscar</h2>
                             </li>
+                        </>
                         ))}
                     </ul>
                 </div>
