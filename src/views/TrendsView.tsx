d19c656085f2b79bbd9e51cfd9b1db0517aaf758
Nicolas Cañete <nicolas.canete@uc.cl>
2024-12-10 00:25:48 -0300

    fix: remove unused import

--- src/views/TrendsView.tsx
+++ src/views/TrendsView.tsx
@@ -1,5 +1,4 @@
 import { useState, useEffect } from "react";
-import { categoriasInverso } from "../../public/categorias";
 import { useAppContext } from "../AppContext";
 import { useNavigate } from "react-router-dom";
 import { getTrends } from "../api/mercadoLibre";
